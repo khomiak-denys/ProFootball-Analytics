9e18c575325b5c6cb2da0828d5658370ee196245
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-04-18 12:29:31 +0300

    refactor(presentation): migrate detail view models to cqrs queries

--- src/ProFootball.Presentation/Converters/TrendToPointCollectionConverter.cs
+++ src/ProFootball.Presentation/Converters/TrendToPointCollectionConverter.cs
@@ -1,7 +1,7 @@
 using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media;
-using ProFootball.Application.Contracts.Analytics;
+using ProFootball.Application.Player.Dtos;
 
 namespace ProFootball.Presentation.Converters;
 
